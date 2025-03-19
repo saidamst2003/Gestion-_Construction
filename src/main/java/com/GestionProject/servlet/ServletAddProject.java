0bb7ff298cecc02f7290d5c3fb6d11bd04ac92a2
saidamst2003 <saida2003mst@gmail.com>
2025-03-19 12:30:42 +0000

    j'ai récupérer les entrées de l'utisateur

--- src/main/java/com/GestionProject/servlet/ServletAddProject.java
+++ src/main/java/com/GestionProject/servlet/ServletAddProject.java
@@ -1,1 +1,35 @@
+package com.GestionProject.servlet;
 
+import com.GestionProject.model.Projet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Date;
+
+
+@WebServlet("/AddProjet")
+public class ServletAddProject extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+            String nom = req.getParameter("nomProjet:");
+            String description = req.getParameter("description");
+            String dateDebut =req.getParameter(" dateDebut");
+            Date dateDebuts =Date.valueOf(dateDebut);
+            String dateFin =req.getParameter("dateFin");
+            Date datefin =Date.valueOf(dateFin);
+            String budget  = req.getParameter("budget");
+            Double Budget = Double.valueOf(budget);
+            
+
+        Projet projet = new Projet(nom,description,dateDebuts,datefin,Budget);
+
+           }
+
+
+}
